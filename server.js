cb894a6f233af67d71d30f1446f2e1e319adb16f
NPT-Dark <tainguyenit24012000@gmail.com>
2022-10-03 15:49:20 +0700

    change port to 5000

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use('/api/song',work)
 //connect to db
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
-  app.listen(5000, () => {  
-    console.log(`Server is running on port: 4000`);
+  app.listen(process.env.PORT || 5000, () => {  
+    console.log(`Server is running on port: 5000`);
 });
 })
 .catch((err)=>{console.log(err)})
